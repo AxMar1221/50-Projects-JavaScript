4262df2a6d517ccb990c086aac932e1d5168cae1
Tachi Dito <tachidito_7inc@hotmail.co>
2023-03-02 13:22:33 -0600

    15

---
+++ 15-incrementing-counter/index.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', () => {
+    console.log('count followers');
+});
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach( counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText
+
+        const increment = target / 100000
+
+        if ( c < target ) {
+            counter.innerText = `${Math.ceil( c + increment) }`
+            setTimeout( updateCounter, 1 );
+        } else {
+            counter.innerText = target
+        }
+    }
+    updateCounter()
+});
+
+setInterval('location.reload()', 60000 );
+
