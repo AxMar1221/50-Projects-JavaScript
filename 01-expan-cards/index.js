dc5c32d1968976186c62d45fb3df58447db3efdc
Tachi Dito <tachidito_7inc@hotmail.co>
2023-03-02 10:14:41 -0600

    01

---
+++ 01-expan-cards/index.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+  console.log("Window loaded");
+});
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
+
+setInterval("location.reload()", 60000);
