95cbbacc9f77f0e0d0919c821f07759fad40bdb0
Tachi Dito <tachidito_7inc@hotmail.co>
2023-03-02 11:08:24 -0600

    09

---
+++ 09-sound-board/index.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', () => {
+    console.log('Window loaded')
+});
+
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    });
+
+    document.getElementById('buttons').appendChild(btn)
+});
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
+
+setInterval('location.reload()', 600000 );
